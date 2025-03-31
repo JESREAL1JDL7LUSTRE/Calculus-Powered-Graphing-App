679a97641634a2d6dec80e1c1909dde83095629c
Jesreal D. Lustre <115926421+JESREAL1JDL7LUSTRE@users.noreply.github.com>
2025-03-31 13:00:03 +0800

    switch from PyQt to PySide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import random
-from PyQt6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtCore, QtWidgets, QtGui
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
@@ -10,7 +10,7 @@ class MyWidget(QtWidgets.QWidget):
 
         self.button = QtWidgets.QPushButton("Click me!")
         self.text = QtWidgets.QLabel("Hello World",
-                                    alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+                                    alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
@@ -18,10 +18,10 @@ class MyWidget(QtWidgets.QWidget):
 
         self.button.clicked.connect(self.magic)
 
-    @QtCore.pyqtSlot()
+    @QtCore.Slot()
     def magic(self):
         self.text.setText(random.choice(self.hello))
-
+        
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
